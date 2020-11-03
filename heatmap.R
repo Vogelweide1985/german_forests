00454b0236a812a37d49497c683b8c4395b90228
vogelweide1985 <vogelweide1985@hotmail.de>
2020-11-03 19:29:05 +0100

    neater heatmap for export

--- heatmap.R
+++ heatmap.R
@@ -1,14 +1,24 @@
 library(gplots)
 library(ggplot2)
 library(tidyr)
+library(RColorBrewer)
 source("config.R", encoding = "UTF-8")
 
+color_start <- "#FAD78A"
+color_mid <- "#772424"
+color_end <- "#B7455B"
+color_test <- "#39325B"
+color_bg <- "#F1F7FF"
+color_style <- c(color_start, color_mid, color_test)
+#color_end <- "#39325B"
 
 #read 
 df <- readRDS(config$rds_name)
 
 #Heatmap Stats
 heat <- as.matrix(df[, -which(names(df) %in% c("Wuchsgebiet"))])
+
+#basic
 heatmap(heat, hclustfun = function(x) hclust(x, method="ward.D"),
         labRow = df$Wuchsgebiet)
 
@@ -24,6 +34,16 @@ ggplot(df_heat_long, aes(Baumart, Wuchsgebiet, fill= df)) +
 d <- dist(heat)
 dend <- hclust(d, method = "ward.D")
 
-heatmap.2(heat, breaks = 10, col = colorspace::sequential_hcl,
-          hclustfun = function(x) hclust(x, method="ward.D"))
+par(bg = color_bg)
+color_visme <- colorRampPalette(color_style)
+heatmap.2(heat, breaks = 10, col = color_visme, dendrogram = "row",
+          hclustfun = function(x) hclust(x, method="ward.D"), margins = c(1,10),
+          cexRow = 0.3, trace = "none", labRow = T, labCol = NA, tracecol = color_test, 
+          key.title = NA, key.xlab = NA , key.ylab = NA,  key.ytickfun = NA)
 
+
+dev.off()
+windows.options(reset=TRUE)
+
+library(purrr)
+as_mapper()
