01bc21d240ebc281cd1d4479472590241f45d7d7
vogelweide1985 <vogelweide1985@hotmail.de>
2020-11-04 12:02:12 +0100

    changed config path

--- geodata.r
+++ geodata.r
@@ -1,13 +1,18 @@
 # Geodata
 library(sf)
+library(ggplot2)
 source("config.R", encoding = "UTF-8")
 
 df <- readRDS(config$rds_name)
 
 
-regions<- st_read("geodata/wuchsgebiete_2011.shp")
+regions<- st_read(config[["shape_path"]])
 
 ggplot() + 
    geom_sf(data = regions, color = "black", fill = "cyan1") + 
    ggtitle("AOI Boundary Plot") + 
-   coord_sf()
+   coord_sf() + 
+   theme(panel.background = element_rect(fill = color_bg,
+                                      colour = color_bg))
+
+         
